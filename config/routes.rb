4045f2cc23948cd190d6a9f5c6d37d829a99ae85
Your Name <you@example.com>
2019-02-04 23:39:53 +0900

    [Start]3-6

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   get 'posts/new'
+
 # /topというURLにアクセスしたときに、rootコントローラのtopアクションが呼び出される
   get '/top' => 'root#top'
-
+# /postsというURLにpostでアクセスしたときに
+#postsコントローラのcreateアクションが呼び出されるように設定
   post '/posts' => 'posts#create'
+
+  get '/posts' => 'posts#index'
 end
