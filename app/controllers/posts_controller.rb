c4fba34a1bcc3933445147ccb8f0456155211149
Your Name <you@example.com>
2019-02-04 23:35:50 +0900

    [Finish]3-5

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,23 @@
 class PostsController < ApplicationController
   def new
+  	# 空のモデルをビューへ渡す
+  	# @のついているインスタンス変数はビューファイルへ受け渡せる
+  	@post = Post.new
+  end
+
+  def  create
+  	#ストロングパラメータを使用・ローカル変数はビューファイルへ受け渡せない。アクション内でしか利用できない
+  	post = Post.new(post_params)
+  	#DBに保存する
+  	post.save
+  	#トップ画面へリダイレクト
+  	redirect_to '/top'
+  end
+
+# 境界線 「ここから下はcontrollerの中でしか呼び出せません」
+  private
+
+  def post_params
+  	params.require(:post).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'posts/new'
+# /topというURLにアクセスしたときに、rootコントローラのtopアクションが呼び出される
   get '/top' => 'root#top'
 
+  post '/posts' => 'posts#create'
 end
