e0e8deb9e44f5a1b43633a7c78e14f9bd9a5a7d8
Your Name <you@example.com>
2019-02-05 19:04:14 +0900

    [Finish]1-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '16263a2675582fd756bd1bb8fa8787cf9da483a2f9fff31667fbb8cebf20aa19ca5d834d8427c138e1302d720d557210aa2fc4aef3f93dfb9b35df4834c26956'
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_04_124719) do
+ActiveRecord::Schema.define(version: 2019_02_05_090428) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_id"
   end
 
 end
